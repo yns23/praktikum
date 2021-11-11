8aa13010e644822c56a931dfcfda56ba581ca627
yns23 <91956023+yns23@users.noreply.github.com>
2021-11-11 11:40:27 +0100

    Initial commit

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,19 @@
+package de.hfu;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextLine().toUpperCase());
+        in.close();
+    }
+}
